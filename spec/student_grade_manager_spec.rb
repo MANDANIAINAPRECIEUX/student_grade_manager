262b03c3e02aec77258ecd0026ba37f7221be1b5
unknown <mandaniainaprecieux@yahoo.com>
2026-07-28 10:48:38 +0300

    chore: initialize student_grade_manager gem

---
+++ spec/student_grade_manager_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe StudentGradeManager do
+  it "has a version number" do
+    expect(StudentGradeManager::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
